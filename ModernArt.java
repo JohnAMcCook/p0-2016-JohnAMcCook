a517e90741393bcf36955e9f6487547571da8998
JohnAMcCook <john.cook@mtchs.org>
2016-09-09 08:20:59 -0600

    Made the modern art
    used println and print and made the project

---
+++ ModernArt.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+
+/**
+ * @author john.cook
+ *
+ */
+public class ModernArt {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("==============");
+		System.out.print("==============");
+		System.out.println("============");
+		System.out.println("|                   |                  |");
+		System.out.println("|-------------------|------------------|");
+		System.out.println("|         |         |                  |");
+		System.out.println("|         |         |                  |");
+		System.out.println("|         |     -----------------      |");
+		System.out.println("|---      |     |       |       |      |");
+		System.out.println("|  |----  |     |       |       |      |");
+		System.out.println("|      |--------|       |       -------|");
+		System.out.println("|  |----        |       |              |");
+		System.out.println("|---            |       |              |");
+		System.out.println("|               ---------              |");
+		System.out.println("|                 |                    |");
+		System.out.println("|                 |                    |");
+		System.out.println("|           -------                    |");
+		System.out.println("|           |                          |");
+		System.out.println("========================================");
+	}
+
+}
